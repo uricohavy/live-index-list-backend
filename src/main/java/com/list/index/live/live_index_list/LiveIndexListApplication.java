bba050d4ad9b42d6579036468bd0867dbaa16a9c
uricohavy <uricohavy@gmail.com>
2025-02-03 12:46:26 -0800

    Initial commit for Spring INitializer

---
+++ src/main/java/com/list/index/live/live_index_list/LiveIndexListApplication.java
@@ -0,0 +1,13 @@
+package com.list.index.live.live_index_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveIndexListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveIndexListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/list/index/live/live_index_list/LiveIndexListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.index.live.live_index_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveIndexListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

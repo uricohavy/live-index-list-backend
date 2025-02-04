dac61f7d1b303bba190693f02bad7443a33f687c
uricohavy <uricohavy@gmail.com>
2025-02-03 16:52:12 -0800

    first actual commit

--- src/main/java/com/list/index/live/live_index_list/LiveIndexListApplication.java
+++ src/main/java/com/list/index/live/live_index_list/LiveIndexListApplication.java
@@ -3,11 +3,11 @@ package com.list.index.live.live_index_list;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LiveIndexListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiveIndexListApplication.class, args);
 	}
-
 }
